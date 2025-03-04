e0a42faafe0f8a59e6d42ba63c40b75d7dde2849
LuizKK12 <115504242+luizkk12@users.noreply.github.com>
2025-03-04 15:24:30 -0300

    Update ping.js

--- src/commands/Utility 🎈/ping.js
+++ src/commands/Utility 🎈/ping.js
@@ -1,1 +1,16 @@
+const { EmbedBuilder } = require('discord.js');
 
+module.exports = {
+    data: {
+    name: "ping",
+    description: "Mostra a latência do bot."
+},
+    async execute(message, args) {
+    let embed = new EmbedBuilder()
+   .setColor('#FF0000')
+   .setTitle('🏓 **| Pong!**')
+    .setDescription(`A minha latência atual é: **__${message.client.ws.ping}__ms**`)
+    
+    message.reply({ embeds: [embed] })
+}
+};
